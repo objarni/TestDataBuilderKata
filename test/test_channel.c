94be5b2bd1b4bc3fac7659e7ba426813b84e68d8
Typist <typist@example.com>
2021-12-01 09:19:09 +0000

    Better test name

--- test/test_channel.c
+++ test/test_channel.c
@@ -4,6 +4,18 @@ Describe(Channel);
 BeforeEach(Channel) {}
 AfterEach(Channel) {}
 
-Ensure(Channel, test_1) {
+Ensure(Channel, the_default_channel) {
     assert_that(1, is_equal_to(1));
 }
+
+/*
+    SECTION("the default channel") {
+        Channel channel = buildChannel(aChannel());
+        REQUIRE(channel.port == 1);
+        REQUIRE(channel.protocol == LEGACY_TCP);
+        REQUIRE(channel.address.bytes[0] == 127);
+        REQUIRE(channel.address.bytes[1] == 0);
+        REQUIRE(channel.address.bytes[2] == 0);
+        REQUIRE(channel.address.bytes[3] == 1);
+    }
+ */
